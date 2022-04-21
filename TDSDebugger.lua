727cec58bd752ece1c90330cfa1c8832613b6d9b
Powershell <61961343+powershell1@users.noreply.github.com>
2022-04-21 17:21:35 +0700

    Update TDSDebugger.lua

--- TDSDebugger.lua
+++ TDSDebugger.lua
@@ -2,5 +2,5 @@ repeat
     wait()
 until game:IsLoaded()
 wait(5)
-loadstring(game:HttpGet("https://f3f5bdf5-b55f-48de-a8b5-3fed720d1d7e.id.repl.co/get-data/pum.tools.co.lua"))()
+--loadstring(game:HttpGet("https://f3f5bdf5-b55f-48de-a8b5-3fed720d1d7e.id.repl.co/get-data/pum.tools.co.lua"))()
 return loadstring(game:HttpGet("https://raw.githubusercontent.com/banbuskox/dfhtyxvzexrxgfdzgzfdvfdz/main/ckmhjvskfkmsStratFun2"))()
