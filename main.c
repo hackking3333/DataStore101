09619b89c5f8055d0d828acfc53db64fa4474834
Powershell <61961343+powershell1@users.noreply.github.com>
2022-06-08 05:06:12 +0000

    main

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!");
+    char s1,s2,s3;
+    printf("Enter s1, s2, s3: ");
+    scanf("%c%c%c",&s1,&s2,&s3);
+    printf("ascii is : %d%d%d\n",s1,s2,s3);
+    printf("s1: %d, s2: %d, s3: %d\n",s1,s2,s3);
     return 0;
 };
